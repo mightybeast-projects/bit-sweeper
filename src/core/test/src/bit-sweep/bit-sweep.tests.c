f7d8356ada19cd78d87d18ea49a50811573e900f
Єфремов Євгеній Олексійович <Yevgenij.Yefremov@feofaniya.lan>
2025-07-31 15:34:01 +0300

    -feat- : Bitsweep default values.

--- src/core/test/src/bit-sweep/bit-sweep.tests.c
+++ src/core/test/src/bit-sweep/bit-sweep.tests.c
@@ -19,7 +19,32 @@ void Bit_Sweep_Allocation_Should_Allocate_New_Bit_Sweep()
     TEST_ASSERT_NOT_NULL(bitSweep);
 }
 
+void Allocated_Bit_Sweep_Should_Have_Initialized_Width()
+{
+    TEST_ASSERT_EQUAL_INT(10, bitSweep->width);
+};
+
+void Allocated_Bit_Sweep_Should_Have_Initialized_Height()
+{
+    TEST_ASSERT_EQUAL_INT(10, bitSweep->height);
+};
+
+void Allocated_Bit_Sweep_Should_Have_Initialized_Bomb_Count()
+{
+    TEST_ASSERT_EQUAL_INT(10, bitSweep->bombCount);
+};
+
+void Allocated_Bit_Sweep_Should_Have_Initialized_Cells()
+{
+    TEST_ASSERT_NOT_NULL(bitSweep->cells);
+};
+
 void testBitSweep()
 {
     RUN_TEST(Bit_Sweep_Allocation_Should_Allocate_New_Bit_Sweep);
+
+    RUN_TEST(Allocated_Bit_Sweep_Should_Have_Initialized_Width);
+    RUN_TEST(Allocated_Bit_Sweep_Should_Have_Initialized_Height);
+    RUN_TEST(Allocated_Bit_Sweep_Should_Have_Initialized_Bomb_Count);
+    RUN_TEST(Allocated_Bit_Sweep_Should_Have_Initialized_Cells);
 }
