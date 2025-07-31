64563bd4cf24985d0c55899153bc4af9415f0a8f
Єфремов Євгеній Олексійович <Yevgenij.Yefremov@feofaniya.lan>
2025-07-31 16:45:09 +0300

    -feat- : Bitsweep bombs init.

--- src/core/src/bit-sweep/bit-sweep.c
+++ src/core/src/bit-sweep/bit-sweep.c
@@ -1,9 +1,21 @@
 #include "bit-sweep.h"
+#include "stdbool.h"
+#include "stdio.h"
 #include "stdlib.h"
+#include "time.h"
 
 #define WIDTH 10
 #define HEIGHT 10
 #define BOMB_COUNT 10
+
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+static void initializeCells(BitSweep* bitSweep);
+
+static void initializeBombIndexes(const BitSweep* bitSweep, int arr[]);
+
+static void assignCellValues(BitSweep* bitSweep, int bombIndexes[]);
 
 BitSweep* allocateBitSweep()
 {
@@ -32,6 +44,8 @@ BitSweep* allocateBitSweep()
             bitSweep->cells[i][j] = allocateCell();
     }
 
+    initializeCells(bitSweep);
+
     return bitSweep;
 }
 
@@ -40,13 +54,85 @@ void freeBitSweep(BitSweep* bitSweep)
     for (int i = 0; i < WIDTH; i++)
     {
         for (int j = 0; j < HEIGHT; j++)
-        {
             freeCell(bitSweep->cells[i][j]);
-        }
 
         free(bitSweep->cells[i]);
     }
 
     free(bitSweep->cells);
     free(bitSweep);
+}
+
+static void initializeCells(BitSweep* bitSweep)
+{
+    srand(time(NULL));
+
+    int* bombIndexes = malloc(sizeof(int) * bitSweep->bombCount);
+
+    if (!bombIndexes)
+        return;
+
+    initializeBombIndexes(bitSweep, bombIndexes);
+    assignCellValues(bitSweep, bombIndexes);
+
+    free(bombIndexes);
+}
+
+static void initializeBombIndexes(const BitSweep* bitSweep, int arr[])
+{
+    for (int i = 0; i < bitSweep->bombCount; i++)
+    {
+        int rIndex = rand() % (bitSweep->width * bitSweep->height);
+
+        bool duplicated = false;
+
+        for (int j = 0; j < bitSweep->bombCount; j++)
+        {
+            if (arr[j] == rIndex)
+            {
+                duplicated = true;
+                break;
+            }
+        }
+
+        if (duplicated)
+        {
+            i--;
+            continue;
+        }
+
+        arr[i] = rIndex;
+    }
+}
+
+static void assignCellValues(BitSweep* bitSweep, int bombIndexes[])
+{
+    for (int i = 0; i < bitSweep->width; i++)
+    {
+        for (int j = 0; j < bitSweep->height; j++)
+        {
+            Cell* cell = bitSweep->cells[i][j];
+
+            cell->x = i;
+            cell->y = j;
+
+            int index = bitSweep->height * i + j;
+
+            for (int k = 0; k < bitSweep->bombCount; k++)
+                if (bombIndexes[k] == index)
+                    cell->value = BOMB;
+
+            const int minX = MAX(0, i - 1);
+            const int maxX = MIN(i + 1, bitSweep->width - 1);
+
+            const int minJ = MAX(0, j - 1);
+            const int maxJ = MIN(j + 1, bitSweep->height - 1);
+
+            for (int k = minX; k <= maxX; k++)
+                for (int l = minJ; l <= maxJ; l++)
+                    addNeighbour(cell, bitSweep->cells[k][l]);
+
+            syncValue(cell);
+        }
+    }
 }
--- src/core/test/src/bit-sweep/bit-sweep.tests.c
+++ src/core/test/src/bit-sweep/bit-sweep.tests.c
@@ -39,6 +39,31 @@ void Allocated_Bit_Sweep_Should_Have_Initialized_Cells()
     TEST_ASSERT_NOT_NULL(bitSweep->cells);
 };
 
+void Allocated_Bit_Sweep_Should_Have_Randomized_Bomb_Cells()
+{
+    int bombCount = 0;
+
+    for (int i = 0; i < bitSweep->width; i++)
+        for (int j = 0; j < bitSweep->height; j++)
+            if (bitSweep->cells[i][j]->value == BOMB)
+                bombCount++;
+
+    TEST_ASSERT_EQUAL_INT(bitSweep->bombCount, bombCount);
+}
+
+void Allocated_Bit_Sweep_Should_Have_Some_Cells_With_Non_Zero_Values()
+{
+    bool nonZeroValue = false;
+
+    for (int i = 0; i < bitSweep->width; i++)
+        for (int j = 0; j < bitSweep->height; j++)
+            if (bitSweep->cells[i][j]->value != ZERO
+                && bitSweep->cells[i][j]->value != BOMB)
+                nonZeroValue = true;
+
+    TEST_ASSERT_TRUE(nonZeroValue);
+}
+
 void testBitSweep()
 {
     RUN_TEST(Bit_Sweep_Allocation_Should_Allocate_New_Bit_Sweep);
@@ -47,4 +72,7 @@ void testBitSweep()
     RUN_TEST(Allocated_Bit_Sweep_Should_Have_Initialized_Height);
     RUN_TEST(Allocated_Bit_Sweep_Should_Have_Initialized_Bomb_Count);
     RUN_TEST(Allocated_Bit_Sweep_Should_Have_Initialized_Cells);
+
+    RUN_TEST(Allocated_Bit_Sweep_Should_Have_Randomized_Bomb_Cells);
+    RUN_TEST(Allocated_Bit_Sweep_Should_Have_Some_Cells_With_Non_Zero_Values);
 }
